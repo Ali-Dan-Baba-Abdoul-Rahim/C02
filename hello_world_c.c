5f8771a25447fa67b116695b98e9ea1aeb1d8eb2
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-28 14:02:03 +0100

    Create hello_world_c.c

---
+++ hello_world_c.c
@@ -0,0 +1,10 @@
+// Inclusion de la bibliothèque standard pour les entrées/sorties
+#include <stdio.h>
+
+// Fonction principale, point d'entrée du programme
+int main() {
+    // Affiche le texte "Hello, World!" suivi d'un saut de ligne
+    printf("Hello, World!\n");
+    // Indique que le programme s'est terminé avec succès
+    return 0;
+}
