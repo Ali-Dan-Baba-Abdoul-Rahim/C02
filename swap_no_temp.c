173b562beacf42fc27c22649cabc93c460b96f8a
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-28 15:05:45 +0100

    Create swap_no_temp.c

---
+++ swap_no_temp.c
@@ -0,0 +1,22 @@
+// Inclusion de la bibliothèque standard pour les entrées/sorties
+#include <stdio.h>
+
+// Fonction principale, point d'entrée du programme
+int main() {
+    // Déclaration des variables pour stocker les deux nombres
+    int a, b;
+    
+    // Lecture des deux nombres saisis par l'utilisateur
+    scanf("%d %d", &a, &b);
+    
+    // Échange des valeurs sans variable temporaire en utilisant des opérations arithmétiques
+    a = a + b; // a contient la somme de a et b
+    b = a - b; // b prend la valeur initiale de a
+    a = a - b; // a prend la valeur initiale de b
+    
+    // Affichage des valeurs après l'échange
+    printf("Après échange: a = %d, b = %d\n", a, b);
+    
+    // Indique que le programme s'est terminé avec succès
+    return 0;
+}
