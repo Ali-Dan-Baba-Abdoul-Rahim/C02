a9841b39135072324b2430dbe16eef696a27824a
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-28 17:06:12 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -6,10 +6,10 @@ int main() {
     // Lire deux nombres
     scanf("%d %d", &a, &b);
     
-    // Inverser avec XOR
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
+    // Inverser sans variable temporaire
+    a = a + b;    // a contient la somme
+    b = a - b;    // b prend la valeur initiale de a
+    a = a - b;    // a prend la valeur initiale de b
     
     // Afficher le résultat
     printf("Après échange: a = %d, b = %d\n", a, b);
