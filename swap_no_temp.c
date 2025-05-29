73337db4cfe0f8c75a3eac035c10edeb06950ad2
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-29 01:24:59 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -7,9 +7,9 @@ int main() {
     scanf("%d %d", &a, &b);
     
     // Inverser avec logique arithmétique
-    a = a * b;
-    b = a / b;  // b = a_initial
-    a = a / b;  // a = b_initial
+    a = a ^ b;
+    b = a ^ b;  // b = a_initial (car a ^ b ^ b = a)
+    a = a ^ b;  // a = b_initial (car a ^ b ^ a = b)
     
     // Afficher exactement comme demandé
     printf("Après échange: a = %d, b = %d\n", a, b);
