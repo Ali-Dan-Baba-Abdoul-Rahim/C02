e3ba3f12875465e58b6ead4124ad4d6397264ebf
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-29 02:08:41 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    double a_double, b_double;
     int a, b;
     
-    // Lire deux nombres et vérifier si la lecture est réussie
-    if (scanf("%d %d", &a, &b) != 2) {
-        return 1; // Retourner 1 si l'entrée est invalide (non numérique ou incomplète)
-    }
+    // Lire deux nombres (entiers ou réels)
+    scanf("%lf %lf", &a_double, &b_double);
+    
+    // Convertir en entiers (tronquer la partie décimale)
+    a = (int)a_double;
+    b = (int)b_double;
     
     // Inverser avec logique arithmétique
     a = a + b;
@@ -16,5 +19,5 @@ int main() {
     // Afficher exactement comme demandé
     printf("Après échange: a = %d, b = %d\n", a, b);
     
-    return 0; // Succès
+    return 0;
 }
